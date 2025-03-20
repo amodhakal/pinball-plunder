1efd545f4fc68732bb8e16672cd6f914388e21a7
Amodh Dhakal <adhakal2@ncsu.edu>
2025-03-20 00:44:23 -0400

    Added log to the left, defeat screen

--- scripts/Game.gd
+++ scripts/Game.gd
@@ -1,7 +1,9 @@
 extends Node
 
+@onready var ball = $BallPhysics
 @onready var left_flipper = $LeftFlipper
 @onready var right_flipper = $RightFlipper
+var defeatLvl = "res://scenes/Defeat.tscn"
 
 func _input(event):
 	if event.is_action_pressed("flip_left"):
@@ -13,3 +15,9 @@ func _input(event):
 		right_flipper.flip()
 	elif event.is_action_released("flip_right"):
 		right_flipper.release()
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if ( body == ball):
+		get_tree().change_scene_to_file(defeatLvl)
+	
