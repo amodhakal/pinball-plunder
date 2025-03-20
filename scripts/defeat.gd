e8102cf4b2b13048b7c50529d70691a61192d530
Amodh Dhakal <adhakal2@ncsu.edu>
2025-03-20 15:36:46 -0400

    Added audio

--- scripts/defeat.gd
+++ scripts/defeat.gd
@@ -5,8 +5,10 @@ extends Node2D
 var gameLvl = "res://scenes/Game.tscn"
 
 func _ready() -> void:
+	AudioManager.play_defeat_music()
 	PointsLabel.text = str(PointManager.getPoints()) + " points collected"
 
 func _on_play_pressed() -> void:
 	PointManager.resetPoints()
+	AudioManager.play_main_music()
 	get_tree().change_scene_to_file(gameLvl)
