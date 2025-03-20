05b2adcbbc2e4c3b224c30d8b83023ca2c4d34b9
Amodh Dhakal <adhakal2@ncsu.edu>
2025-03-20 18:28:15 -0400

    Fixed point system and added damping

--- scripts/defeat.gd
+++ scripts/defeat.gd
@@ -1,8 +1,10 @@
 extends Node2D
 
 @onready var PlayBtn = $Play
+@onready var HomeBtn = $Home
 @onready var PointsLabel = $Points
 var gameLvl = "res://scenes/Game.tscn"
+var titleLvl = "res://scenes/Title.tscn"
 
 func _ready() -> void:
 	AudioManager.play_defeat_music()
@@ -12,3 +14,8 @@ func _on_play_pressed() -> void:
 	PointManager.resetPoints()
 	AudioManager.play_main_music()
 	get_tree().change_scene_to_file(gameLvl)
+
+
+func _on_button_pressed_home() -> void:
+	PointManager.resetPoints()
+	get_tree().change_scene_to_file(titleLvl)
